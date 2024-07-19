0661145ff8484b338edab868936a5652dbd545e5
SAI PRAVEEN TAMMANA <saipraveen694@gmail.com>
2024-07-19 18:42:22 +0530

    hello

---
+++ sign.py
@@ -0,0 +1,8 @@
+n = int(input("Enter a number: "))
+
+if n > 0:
+    print("{} is a positive number".format(n))
+elif n == 0:
+    print("{} is zero".format(n))
+else:
+    print("{} is a negative number".format(n))
